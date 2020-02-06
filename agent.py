ed54f067097b6d7b2634d0fe2fda47312f493c14
ab5163 <akim.borbuev@gmail.com>
2020-02-06 11:30:23 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -11,7 +11,7 @@ def huber_loss(y_true, y_pred):
 
 class Agent:
     
-    def __init__(self, input_dim, output_dim, lr, gamma, tau, buffer_size, l1_units, l2_units, l3_units):
+    def __init__(self, input_dim, output_dim, lr, gamma, tau, buffer_size, l1_units, l2_units, l3_units, learning_start):
 
         self.memory  = deque(maxlen=buffer_size)
         self.input_dim = input_dim
@@ -19,8 +19,10 @@ class Agent:
         self.actions = range(output_dim)  
         self.gamma = gamma
         self.epsilon = 1.0
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.1
+        self.ls = learning_start
+        self.epsilon_const = self.ls+1000
+        self.epi = 0
         self.learning_rate = lr
         self.tau = tau
         self.l1_units = l1_units
@@ -30,6 +32,11 @@ class Agent:
         self.model, self.init_weights = self.create_model()
         self.target_model, self.target_init_weights = self.create_model()
 
+    def xplr(self):
+        self.epsilon = (self.epsilon_min-1)/self.epsilon_const*self.epi+1
+        self.epsilon = max(self.epsilon, self.epsilon_min)
+        self.epi += 1
+    
     def create_model(self):
         model   = Sequential()
         model.add(Dense(self.l1_units, input_dim = self.input_dim, activation="relu"))
@@ -41,8 +48,7 @@ class Agent:
         return model, init_weights
 
     def act(self, state):
-        self.epsilon *= self.epsilon_decay
-        self.epsilon = max(self.epsilon_min, self.epsilon)
+        self.xplr()
         if np.random.random() < self.epsilon:
             return np.random.choice(self.actions)
         return self.model.predict(state)[0]
@@ -52,6 +58,9 @@ class Agent:
 
     def replay(self):
         batch_size = 32
+        if len(self.memory) < batch_size: 
+            return
+
         states = []
         targets = []
         samples = random.sample(self.memory, batch_size)
@@ -60,10 +69,10 @@ class Agent:
             states.append(state[0])
             target = self.target_model.predict(state)
             if done:
-                target[0][action] = reward
+                target[0][action] = reward/100
             else:
                 Q_future = max(self.target_model.predict(new_state)[0])
-                target[0][action] = np.clip(reward + Q_future * self.gamma, -1, 0)
+                target[0][action] = np.clip(reward/100 + Q_future * self.gamma, -1, 0)
             targets.append(target[0])
         states = np.array(states)
         targets = np.array(targets)
@@ -76,8 +85,10 @@ class Agent:
             target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
         self.target_model.set_weights(target_weights)
         
-    def reset_weights(self,reset_weights):
+    def reset_weights(self, reset_weights):
         if reset_weights:
             self.model.set_weights(self.init_weights)
             self.target_model.set_weights(self.target_init_weights)
         self.memory.clear()
+        self.epsilon = 1.0
+        self.epi = 0
